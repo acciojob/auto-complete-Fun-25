becd40b7c66090f195951b251110da0b7d76771e
Fun-25 <muskandodmani760@gmail.com>
2023-06-25 16:05:37 +0530

    submitting code-Sun Jun 25 2023 16:05:33 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
+import React, { useState,useEffect} from "react";
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
 const App = () => {
+  const [searchTerm, setsearchTerm] = useState("");
+  const [fruits,setFruits]=useState(fruits)
+
+  useEffect(()=>{
+    setFruits(fruits.filter(value=>value.toLowerCase.includes(searchTerm.toLowerCase)))
+  },[search]
+
+  )
   return (
     <div>
-        {/* Do not remove the main div */}
+      <input
+        type="search"
+        placeholder="Search"
+        onChange={(e) => setsearchTerm(e.target.value)}
+      />
+      <ul>
+        {
+          fruits.map(fruit=>{
+            <li>{fruit}</li>
+          })
+        }
+      </ul>
+       
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
